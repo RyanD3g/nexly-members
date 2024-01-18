c25a06af9ae2b1fcea85294eb09dee66fba41979
RyanD3g <durma155@gmail.com>
2024-01-18 15:41:42 -0300

    Remove middleware oAuth

--- src/useCases/producer/OAuth/LoginOauth.module.ts
+++ src/useCases/producer/OAuth/LoginOauth.module.ts
@@ -25,6 +25,6 @@ export class LoginOAuthModule implements NestModule {
     configure(consumer: MiddlewareConsumer) {
         consumer
         .apply(IsJwtMiddleware, isSigned, IsProducer)
-            .forRoutes('/Oauth');
+            .forRoutes('/Oauth/listChannels', '/Oauth/listPlaylists/setChannels', '/Oauth/setPlaylist');
     };
 };
