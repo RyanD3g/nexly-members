858b16200757dbe532adf8a14813eddb8a2c459b
RyanD3g <durma155@gmail.com>
2024-02-01 19:34:50 -0300

    Remove isNotEmpty

--- src/useCases/producer/createACourse/CreateCourse.DTO.ts
+++ src/useCases/producer/createACourse/CreateCourse.DTO.ts
@@ -3,20 +3,15 @@ import { IsBoolean, IsNotEmpty } from "class-validator";
 export class ICreateCourse {
     producerId?:string;
 
-    @IsBoolean()
     certificate?:boolean;
     
-    @IsNotEmpty()
     name:string;
 
     urlThumbCourse:string;
 
-    @IsNotEmpty()
     description:string;
 
-    @IsNotEmpty()
     categorysTag:string[];
 
-    @IsNotEmpty()
     duration:string;
 };
